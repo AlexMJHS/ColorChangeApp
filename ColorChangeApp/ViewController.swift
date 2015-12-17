1c25d5fc3250d57e8c6784e525ee5c437d61b0de
Alejandro <AlexMJHS@gmail.com>
2015-12-17 08:38:03 -0700

    Added ColorChangeApp to repo

--- ColorChangeApp/ViewController.swift
+++ ColorChangeApp/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func changeColorButton(sender: AnyObject)
+    {
+        view.backgroundColor = UIColor.purpleColor()
+    }
+    
+    @IBAction func changeColorSegment(sender: AnyObject)
+    {
+        view.backgroundColor = UIColor.magentaColor()
+    }
+    
+    @IBAction func changeColorSlider(sender: AnyObject)
+    {
+        view.backgroundColor = UIColor.blueColor()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
